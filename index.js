0acbbc1ae8e5a71544326da45c2737a49fd34334
yogeshthakare402 <yogeshthakare402@gmail.com>
2023-02-07 23:33:24 +0530

    changes for image upload

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ mongoose.connect(atlasUrl)
 
 let app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({
+    limit: '50mb'
+  }));
+// app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
